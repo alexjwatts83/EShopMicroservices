33b58b962f1cc13cd6ca8b2eeb93210b31d20435
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-17 10:58:26 +1100

    Added get by id handler, endpoint and also added a product not found exception

---
+++ src/Services/Catalog/Catalog.API/Products/GetById/Handler.cs
@@ -0,0 +1,20 @@
+﻿
+using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+public class QueryByIdHandler(IDocumentSession session, ILogger<QueryByIdHandler> logger) 
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        return product is null 
+            ? throw new ProductNotFoundException() 
+            : new GetProductByIdResult(product);
+    }
+}
