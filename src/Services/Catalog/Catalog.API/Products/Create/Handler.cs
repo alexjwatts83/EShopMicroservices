57e8031caddae04c230adefd883e77b369e3be90
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-16 16:43:48 +1100

    updated the create product endpoint and handler to use Mapster and Carter. Added Global Using

--- src/Services/Catalog/Catalog.API/Products/Create/Handler.cs
+++ src/Services/Catalog/Catalog.API/Products/Create/Handler.cs
@@ -1,5 +1,28 @@
-﻿namespace Catalog.API.Products.Create;
+﻿using Catalog.API.Models;
 
-public class Handler
+namespace Catalog.API.Products.Create;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: change to use AutoMapper
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO: save to db
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
 }
