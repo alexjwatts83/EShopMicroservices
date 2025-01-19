da31e46a5ca5ae0458fa36a0abebfeb0c35c9343
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-19 13:23:04 +1100

    added sql lite and created db with migration and seed data

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,14 +1,30 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/**********************************/
+/* Add services to the container. */
+/**********************************/
+
+// Add Grpc
 builder.Services.AddGrpc();
+
+// Add Sql Lite
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/****************************************/
+/* Configure the HTTP request pipeline. */
+/****************************************/
 
+// Create/Seed/Migrate the SQL Lite Db
+app.UseMigration();
+
+// Add Grpc Services
 app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
