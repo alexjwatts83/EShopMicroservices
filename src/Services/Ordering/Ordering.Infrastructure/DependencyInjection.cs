c84ed4065627e79a006141ba2f284f5cf989359b
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-20 15:35:25 +1100

    Added domain event dispatcher interceptor

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -13,7 +13,7 @@ public static class DependencyInjection
 
         // Add services to the container.
         services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
-        //services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
         services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
