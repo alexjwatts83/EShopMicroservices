503317a6da7871fcc7a864de18c3dc32aa713091
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-22 15:06:28 +1100

    send message when creating an order

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/CreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/CreatedEventHandler.cs
@@ -1,11 +1,15 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+﻿using MassTransit;
 
-public class CreatedEventHandler(ILogger<CreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class CreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<CreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        //throw new NotImplementedException();
+        var orderCreatedIntegrationEvent = domainEvent.Order.ToDto();
+
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/UpdatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/UpdatedEventHandler.cs
@@ -2,10 +2,10 @@
 
 public class UpdatedEventHandler(ILogger<UpdatedEventHandler> logger) : INotificationHandler<OrderUpdatedEvent>
 {
-    public async Task Handle(OrderUpdatedEvent domainEvent, CancellationToken cancellationToken)
+    public Task Handle(OrderUpdatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        //throw new NotImplementedException();
+        return Task.CompletedTask;
     }
 }
