83c7bb8b7c5c53fe1421b5d27f07acb6db2d5af7
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-22 15:14:46 +1100

    Added feature managment for fufilling orders

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/CreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/CreatedEventHandler.cs
@@ -1,12 +1,22 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
-public class CreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<CreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+public class CreatedEventHandler(IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<CreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        // TODO have a key instead of a magic string
+        var orderFullfilmentEnabled = await featureManager.IsEnabledAsync("OrderFullfilment");
+        if (!orderFullfilmentEnabled)
+        {
+            logger.LogInformation("OrderFullfilment Not enabled");
+            return;
+        }
 
         var orderCreatedIntegrationEvent = domainEvent.Order.ToDto();
 
